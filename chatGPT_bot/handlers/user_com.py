3e6388b43ee1bb34089fea575a902967706313c5
totoshkus <totoshkus@gmail.com>
2024-01-17 17:10:45 +0100

    First

--- chatGPT_bot/handlers/user_com.py
+++ chatGPT_bot/handlers/user_com.py
@@ -1,24 +1,19 @@
-from aiogram import F
-from aiogram.types import Message
-import os
-from aiogram import Router
 import db
-import os
 import logging
-from aiogram import Bot, Dispatcher, types
 import asyncio
-import tabulate
 import g4f
 import db
-from collections import deque
-from functools import wraps
 from language import *
-from aiogram import F
+from aiogram.filters.command import Command
+from aiogram import F, Router, F, types
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 # Загрузка переменных окружения
 
 database = db.Database()
 router = Router()
+selected_language = ru_strings
+ADMIN_USER_ID = 6918756613
 
 async def generate_response(user_message):
     try:
@@ -29,8 +24,22 @@ async def generate_response(user_message):
     except Exception as e:
         return str(e)
 
-@router.message(F.text == "/start")
+@router.message(Command("start"))
 async def on_start(message: types.Message):
+    kb = [
+        [
+            types.KeyboardButton(text="Купить токены! 💰"),
+            types.KeyboardButton(text="Профиль 🧑‍💼")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите способ подачи"
+    )
+
+
+
     user_id = message.from_user.id
     username = message.from_user.username
     registration_date = message.date.strftime("%Y-%m-%d")
@@ -38,9 +47,196 @@ async def on_start(message: types.Message):
 
     database.insert_user(user_id, username, registration_date, remaining_tokens)
 
-    await message.answer("Привет!")
+    await message.answer("Привет!", reply_markup=keyboard)
 
-@router.message(lambda message: message.text and not message.text.startswith('/start'))
+from aiogram import types
+
+@router.message(F.text.startswith("Купить токены!"))
+async def without_puree(message: types.Message):
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="20 рублей - 20 токенов 💰",
+        callback_data="20tokens")
+    )
+    builder.add(types.InlineKeyboardButton(
+        text="30 рублей - 30 токенов 💰",
+        callback_data="30tokens")
+    )
+    builder.add(types.InlineKeyboardButton(
+        text="40 рублей - 40 токенов 💰",
+        callback_data="40tokens")
+    )
+    builder.add(types.InlineKeyboardButton(
+        text="70 рублей - 70 токенов 💰",
+        callback_data="70tokens")
+    )
+    await message.answer(
+        "Выберите тариф (1 токен = запрос = 1 рубль):",
+        reply_markup=builder.as_markup()
+    )
+
+@router.message(F.text.startswith("Профиль"))
+async def show_profile(message: types.Message):
+    user_id = message.from_user.id
+    user = database.get_user(user_id)
+    
+    if user:
+        user_id = user[0]
+        username = user[2]
+        remaining_tokens = user[4]
+        
+        emoji_wallet = "💰"  # Эмодзи для токенов
+        profile_text = (
+            f"Профиль пользователя @{username} (ID: {message.from_user.id}):\n"
+            f"Оставшиеся токены: {remaining_tokens} {emoji_wallet}"
+        )
+
+        # Добавим эмодзи для уровня и достижений (ваш код)
+
+        await message.answer(profile_text)
+    else:
+        await message.answer(selected_language['not_registered'])
+
+    
+@router.callback_query(F.data == "20tokens")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.message.answer("Выбрано 20 токенов! 💰")
+
+    thank_you_message = (
+            f"🌟 Спасибо большое за желание приобрести 20 токенов! 🌟\n"
+            f"Чтобы завершить покупку, напишите на @CodeWarpX."
+    )
+
+    await callback.message.answer(thank_you_message)
+
+@router.callback_query(F.data == "30tokens")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.message.answer("Выбрано 30 токенов! 💰")
+
+    thank_you_message = (
+            f"🌟 Спасибо большое за желание приобрести 30 токенов! 🌟\n"
+            f"Чтобы завершить покупку, напишите на @CodeWarpX."
+    )
+
+    await callback.message.answer(thank_you_message)
+
+
+@router.callback_query(F.data == "40tokens")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.message.answer("Выбрано 40 токенов! 💰")
+
+    thank_you_message = (
+            f"🌟 Спасибо большое за желание приобрести 40 токенов! 🌟\n"
+            f"Чтобы завершить покупку, напишите на @CodeWarpX."
+    )
+
+    await callback.message.answer(thank_you_message)
+
+
+@router.callback_query(F.data == "70tokens")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.message.answer("Выбрано 70 токенов! 💰")
+
+    thank_you_message = (
+            f"🌟 Спасибо большое за желание приобрести 70 токенов! 🌟\n"
+            f"Чтобы завершить покупку, напишите на @CodeWarpX."
+    )
+
+    await callback.message.answer(thank_you_message)
+
+@router.message(F.text.startswith('/ban_user'))
+async def ban_user(message: types.Message):
+    if message.from_user.id == ADMIN_USER_ID:
+        command_parts = message.text.split(maxsplit=1)
+        
+        if len(command_parts) > 1:
+            user_id_to_ban = command_parts[1]
+            
+            try:
+                user_id_to_ban = int(user_id_to_ban)
+                user = database.get_user(user_id_to_ban)
+
+                if user:
+                    database.ban_user(user_id_to_ban)
+                    ban_success_message = f"Пользователь {user[2]} с ID {user_id_to_ban} заблокирован администратором."
+                    logging.info(ban_success_message)
+                    await message.answer(ban_success_message)
+                else:
+                    user_not_found_message = "Пользователь не найден."
+                    await message.answer(user_not_found_message)
+            except ValueError:
+                invalid_user_id_message = "Неверный ID пользователя. Пожалуйста, введите числовой ID пользователя."
+                await message.answer(invalid_user_id_message)
+        else:
+            missing_user_id_message = "Не указан ID пользователя. Пожалуйста, введите числовой ID пользователя."
+            await message.answer(missing_user_id_message)
+    else:
+        unban_user_permission_denied_message = "Отсутствует разрешение на блокировку пользователя."
+        await message.answer(unban_user_permission_denied_message)
+
+@router.message(F.text.startswith('/add_tokens'))
+async def add_tokens(message: types.Message):
+    if message.from_user.id == ADMIN_USER_ID:
+        command_parts = message.text.split(maxsplit=2)
+        if len(command_parts) == 3:
+            try:
+                user_id_to_add_tokens, tokens_to_add = map(int, command_parts[1:])
+                user = database.get_user(user_id_to_add_tokens)
+                if user:
+                    remaining_tokens = user[4]
+                    remaining_tokens += tokens_to_add
+                    database.update_tokens(user_id_to_add_tokens, remaining_tokens)
+                    add_tokens_success_message = f"Добавлено {tokens_to_add} токенов пользователю {user[2]} (ID: {user_id_to_add_tokens})."
+                    await message.answer(add_tokens_success_message)
+                else:
+                    user_not_found_message = "Пользователь не найден."
+                    await message.answer(user_not_found_message)
+            except ValueError:
+                invalid_command_format_message = "Неверный формат команды. Используйте /add_tokens [user_id] [количество токенов]."
+                await message.answer(invalid_command_format_message)
+        else:
+            invalid_command_format_message = "Неверный формат команды. Используйте /add_tokens [user_id] [количество токенов]."
+            await message.answer(invalid_command_format_message)
+    else:
+        add_tokens_permission_denied_message = "Отсутствует разрешение на добавление токенов."
+        await message.answer(add_tokens_permission_denied_message)
+
+
+
+from aiogram import types
+
+@router.message(F.text.startswith('/unban_user'))
+async def unban_user(message: types.Message):
+    if message.from_user.id == ADMIN_USER_ID:
+        command_parts = message.text.split(maxsplit=1)
+
+        if len(command_parts) == 2:
+            command, user_id_to_unban = command_parts
+            try:
+                user_id_to_unban = int(user_id_to_unban)
+                user = database.get_user(user_id_to_unban)
+                if user:
+                    database.unban_user(user_id_to_unban)
+                    user_unban_success_message = f"Пользователь {user[2]} с ID {user_id_to_unban} разблокирован администратором."
+                    logging.info(user_unban_success_message)
+                    await message.answer(user_unban_success_message)
+                else:
+                    user_not_found_message = "Пользователь не найден."
+                    await message.answer(user_not_found_message)
+            except ValueError:
+                invalid_user_id_message = "Неверный ID пользователя. Пожалуйста, введите числовой ID пользователя."
+                await message.answer(invalid_user_id_message)
+        else:
+            invalid_command_format_message = "Неверный формат команды. Используйте /unban_user [user_id]."
+            await message.answer(invalid_command_format_message)
+    else:
+        unban_user_permission_denied_message = "Отсутствует разрешение на разблокировку пользователя."
+        await message.answer(unban_user_permission_denied_message)
+
+
+@router.message(lambda message: message.text and not message.text.startswith('/start') and not message.text.startswith('/ban_user')
+                and not message.text.startswith('/unban_user') and not message.text.startswith('/add_tokens') and not message.text.startswith('Купить токены!') 
+                and not message.text.startswith('Профиль'))
 # @rate_limit(RATE_LIMIT_MESSAGES, RATE_LIMIT_PERIOD)
 async def on_text(message: types.Message):
     user_message = message.text
--- chatGPT_bot/main.py
+++ chatGPT_bot/main.py
@@ -1,19 +1,20 @@
 import os
 from dotenv import load_dotenv
 import logging
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, types, Dispatcher, F
 import asyncio
+from io import BytesIO
+from pydub import AudioSegment
+from handlers.user_com import generate_response
 import tabulate
-import g4f
 import db
-from collections import deque
-from functools import wraps
 from language import *
 from handlers import user_com
-from aiogram import F
+import speech_recognition as sr
 
 # Загрузка переменных окружения
 load_dotenv()
+r = sr.Recognizer()
 
 # Выбор языка (здесь вы можете использовать "ru_strings" или "en_strings")
 selected_language = ru_strings
@@ -26,70 +27,11 @@ logging.basicConfig(filename="bot.log", filemode="w", format="%(asctime)s [%(lev
 
 # Константы
 ADMIN_USER_ID = int(os.getenv('ADMIN_USER_ID'))
-TOKENS_PER_DAY = 15
+
 
 # Параметры ограничения по скорости
-RATE_LIMIT_MESSAGES = 4  # Максимальное количество сообщений
-RATE_LIMIT_PERIOD = 60  # Период времени (в секундах)
-
 database = db.Database()
 
-
-
-def is_user_banned(user_id):
-    user = database.get_user(user_id)
-    return user and user[5]
-
-# Создание словаря для отслеживания количества сообщений для каждого пользователя
-user_message_counts = {}
-
-def rate_limit(limit: int, period: int):
-    def decorator(func):
-        @wraps(func)
-        async def wrapped(message: types.Message, *args, **kwargs):
-            user_id = message.from_user.id
-
-            # Проверка, заблокирован ли пользователь
-            if is_user_banned(user_id):
-                await message.answer(selected_language['user_banned_message'])
-                # Запись блокировки пользователя
-                log_message = f"Пользователь {message.from_user.username} (ID: {user_id}) заблокирован из-за превышения ограничения по скорости."
-                logging.info(log_message)
-                # Уведомление администратора о блокировке пользователя
-                await notify_administrator(message.from_user.username, user_id)
-                return
-
-            # Инициализация подсчета сообщений для пользователя, если еще не была проведена
-            if user_id not in user_message_counts:
-                user_message_counts[user_id] = deque(maxlen=limit)
-
-            # Проверка, отправил ли пользователь слишком много сообщений в пределах ограничения по скорости
-            current_time = asyncio.get_event_loop().time()
-            if len(user_message_counts[user_id]) >= limit:
-                earliest_timestamp = user_message_counts[user_id][0]
-                if current_time - earliest_timestamp < period:
-                    await message.answer(selected_language['user_banned_message'])
-                    # Запись блокировки пользователя
-                    log_message = f"Пользователь {message.from_user.username} (ID: {user_id}) заблокирован из-за превышения ограничения по скорости."
-                    logging.info(log_message)
-                    database.ban_user(user_id)  # Блокировка пользователя
-                    # Уведомление администратора о блокировке пользователя
-                    await notify_administrator(message.from_user.username, user_id)
-                    return
-
-            # Добавление текущей метки времени в список
-            user_message_counts[user_id].append(current_time)
-
-            # Вызов декорированной функции
-            await func(message, *args, **kwargs)
-
-        return wrapped
-
-    return decorator
-
-async def notify_administrator(username, user_id):
-    admin_notification = selected_language['ban_notification'].format(username=username, user_id=user_id)
-    await bot.send_message(ADMIN_USER_ID, admin_notification)
 
 @dp.message(F.text == '/list_users')
 async def list_users(message: types.Message):
@@ -101,102 +43,45 @@ async def list_users(message: types.Message):
     else:
         await message.answer(selected_language['list_users_permission_denied'])
 
-@dp.message(F.text == '/ban_user')
-async def ban_user(message: types.Message):
-    if message.from_user.id == ADMIN_USER_ID:
-        user_id_to_ban = message.get_args()
-        
-        user_id_to_ban = int(user_id_to_ban)
-        user = database.get_user(user_id_to_ban)
-        if user:
-            database.ban_user(user_id_to_ban)
-            await message.answer(selected_language['user_ban_success'].format(username=user[2]))
-
-                # Запись блокировки пользователя
-            log_message = f"Пользователь {user[2]} с ID {user_id_to_ban} заблокирован администратором."
-            logging.info(log_message)
-
-        else:
-            await message.answer(selected_language['user_not_found'])
+@dp.message(F.voice)
+async def audio_message_handler(msg: types.Message):
+    try:
+       file_in_io = BytesIO()
+       file = await bot.get_file(msg.voice.file_id)
+       file_path = file.file_path
+       
+       print("Ok")
+       await bot.download_file(file_path, destination=file_in_io)
+       file_in_io.seek(0)
+       
+       voice_msg_text = voice_recognize(ogg_to_wav(file_in_io))
+       
+       await msg.reply(f"Распознанно: '{voice_msg_text}'")
     
-    else:
-        await message.answer(selected_language['unban_user_permission_denied'])
+       await msg.reply(await generate_response(voice_msg_text))
+    except Exception as e:
+        print(f"Ошибка: {e}")
+    
+def ogg_to_wav(ogg_data):
+    audio = AudioSegment.from_file(ogg_data, format='ogg')
+    wav_data = BytesIO()
+    audio.export(wav_data, format='wav')
+    wav_data.seek(0)
+    return wav_data
 
-@dp.message(F.text == '/unban_user')
-async def unban_user(message: types.Message):
-    if message.from_user.id == ADMIN_USER_ID:
-        user_id_to_unban = message.get_args()
-        try:
-            user_id_to_unban = int(user_id_to_unban)
-            user = database.get_user(user_id_to_unban)
-            if user:
-                database.unban_user(user_id_to_unban)
-                await message.answer(selected_language['user_unban_success'].format(username=user[2]))
-
-                # Запись разблокировки пользователя
-                log_message = f"Пользователь {user[2]} с ID {user_id_to_unban} разблокирован администратором."
-                logging.info(log_message)
-
-            else:
-                await message.answer(selected_language['user_not_found'])
-        except ValueError:
-            await message.answer(selected_language['invalid_user_id'].format(command="unban_user"))
-    else:
-        await message.answer(selected_language['unban_user_permission_denied'])
-
-@dp.message(F.text == '/add_tokens')
-async def add_tokens(message: types.Message):
-    if message.from_user.id == ADMIN_USER_ID:
-        args = message.get_args().split()
-        if len(args) == 2:
-            user_id_to_add_tokens, tokens_to_add = map(int, args)
-            user = database.get_user(user_id_to_add_tokens)
-            if user:
-                remaining_tokens = user[4]
-                remaining_tokens += tokens_to_add
-                database.update_tokens(user_id_to_add_tokens, remaining_tokens)
-                await message.answer(selected_language['add_tokens_success'].format(tokens_to_add=tokens_to_add, username=user[2]))
-            else:
-                await message.answer(selected_language['user_not_found'])
-        else:
-            await message.answer(selected_language['invalid_user_id'].format(command="add_tokens"))
-    else:
-        await message.answer(selected_language['add_tokens_permission_denied'])
-
-@dp.message(lambda message: message.text == selected_language['buy_tokens_prompt'])
-async def buy_tokens(message: types.Message):
-    await message.answer(selected_language['buy_tokens_prompt'])
-    # Теперь давайте дождемся ответа пользователя.
-
-@dp.message(F.text == '/Check_tokens')
-async def check_tokens(message: types.Message):
-    user_id = message.from_user.id
-    user = database.get_user(user_id)
-
-    if user:
-        remaining_tokens = user[4]
-        await message.answer(selected_language['check_tokens_message'].format(remaining_tokens=remaining_tokens))
-    else:
-        await message.answer(selected_language['not_registered'])
-
-def add_tokens_daily():
-    users = database.get_all_users()
-    for user in users:
-        user_id = user[1]
-        remaining_tokens = user[4]
-        remaining_tokens += TOKENS_PER_DAY
-        database.update_tokens(user_id, remaining_tokens)
-
-# Расписание для ежедневного добавления токенов
+def voice_recognize(voice: BytesIO) -> str:
+    voice_rec = sr.AudioFile(voice)
+    with voice_rec as source:
+        audio = r.record(source)
+    return r.recognize_google(audio, language="ru-RU")
 
 
 async def main():
     dp.include_router(
         user_com.router
     )
-
+    
     await dp.start_polling(bot)
-
 
 if __name__ == '__main__':
     asyncio.run(main())
